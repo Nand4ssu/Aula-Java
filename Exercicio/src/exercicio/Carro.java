383d95583ffd7569eafe9e23a73a15786e750615
Nand.Devs <maaay.amanda@gmail.com>
2025-03-19 20:16:19 -0300

    Aula de DS.
    Atualizando o exercicio do Carro.

---
+++ Exercicio/src/exercicio/Carro.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercicio;
+
+/**
+ *
+ * @author AMANDA_4800
+ */
+public class Carro {
+    String marca;
+    String modelo;
+    String cor;
+    String combustivel;
+    
+   
+}
---
+++ Exercicio/src/exercicio/Exercicio.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicio;
+
+/**
+ *
+ * @author AMANDA_4800
+ */
+public class Exercicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Carro carroA = new Carro();
+        Carro carroB = new Carro ();
+        
+        carroA.cor= "Rosa";
+        carroA.marca = "Mercedes";
+        carroA.modelo = "AMG A 35 4MATIC";
+        carroA.combustivel = "Diesel";
+       
+        System.out.println(carroA.combustivel+"\n"+carroA.cor+"\n"+carroA.marca+"\n"+carroA.modelo);
+        
+        System.out.println("===========================");
+        
+        carroB.combustivel = "Gasolina";
+        carroB.cor = "Preto";
+        carroB.marca = "Honda";
+        carroB.modelo = "Civic";
+        System.out.println(carroB.combustivel+"\n"+carroB.cor+"\n"+carroB.marca+"\n"+carroB.modelo);
+    }
+    
+}
